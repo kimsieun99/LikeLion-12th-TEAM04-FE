53fd1930976c99c9106c1e8167530019869c222f
kimsieun99 <dotoryeop@gmail.com>
2024-07-25 02:41:14 +0900

    Feat. Section2.jsx, Section2.css 완성

--- myibo/src/App.js
+++ myibo/src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import Nav from './components/Nav';
-import './App.css';
-import Footer from './components/Footer';
+import React from "react";
+// import Nav from "./components/Nav";
+import "./App.css";
+import Section2 from "./components/Section2";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
+    <>
+      <Section2 />
       <Footer />
-    </div>
+    </>
   );
 }
 
