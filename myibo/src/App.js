65cbd2ea082a1c3480ee558c87e688e673843db5
shtfh <syj2003826@gmail.com>
2024-07-26 12:54:47 +0900

    Design.section1 css 수정

--- myibo/src/App.js
+++ myibo/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Nav from "./components/Nav";
 import "./App.css";
+import Nav from "./components/Nav";
 import Section1 from "./components/Section1";
 import Section2 from "./components/Section2";
 import Section3 from "./components/Section3";
