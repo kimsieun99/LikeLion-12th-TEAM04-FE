6ea525722a4dcc338143782c016629c0bf48f4e7
shtfh <syj2003826@gmail.com>
2024-08-05 20:26:56 +0900

    feat.MyPage 완성, 파일구조 정리

--- myibo/src/App.js
+++ myibo/src/App.js
@@ -1,38 +1,30 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./App.css";
 import Nav from "./components/Nav";
-import Section1 from "./components/Section1";
-import Section2 from "./components/Section2";
-import Section3 from "./components/Section3";
-import Section4 from "./components/Section4";
 import Footer from "./components/Footer";
 import MyPage from "./components/MyPage";
 import SignupPage from "./components/SignupPage";
 import LoginPage from "./components/LoginPage";
+import MainPage from "./pages/MainPage";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
+    <>
+      <Router>
         <Nav />
         <Routes>
-          <Route path="/" element={
-            <>
-              <Section1 />
-              <Section2 />
-              <Section3 />
-              <Section4 />
-            </>
-          } />
-          <Route path="/MyPage" element={<MyPage />} />
+          <Route path="/" element={<MainPage />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/login" element={<LoginPage />} /> 
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+          <Route path="/MyPage" element={<MyPage />} /> 
+      </Routes>
+      <Footer />
+      </Router>
+      
+    </>
   );
+    
+
 }
 
 export default App;
