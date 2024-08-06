db01fb4780655e00916cb4de95dc75a5c6eb437e
kimsieun99 <dotoryeop@gmail.com>
2024-08-06 22:52:54 +0900

    myibo

--- myibo/src/components/MyPage.jsx
+++ myibo/src/components/MyPage.jsx
@@ -8,12 +8,11 @@ const MyPage = () => {
       <p className="insurance-info">회원님의 보험정보</p>
       <main className="mypage-main">
         <Link to="/my-insurance">
-          <a href="/">a</a>
+          <a href="/">내가 가입한 보험 정보 보러가기🎶</a>
         </Link>
       </main>
     </div>
   );
 };
-
 
 export default MyPage;
